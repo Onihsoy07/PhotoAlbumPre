a740e7a050e0179bd44eea0a8af61de5b68e3cb7
Onihsoy07 <pjh2000630@naver.com>
2023-02-23 09:25:16 +0900

    add AlbumRepository

--- photoalbum/src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
+++ photoalbum/src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
@@ -1,5 +1,10 @@
 package com.squarecross.photoalbum.repository;
 
-public interface AlbumRepository {
+import com.squarecross.photoalbum.domain.Album;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlbumRepository extends JpaRepository<Album, Long> {
 
 }
